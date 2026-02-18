92ee4dddc8970c14ffb12845c2d841de751f6880
kemji002 <kenji.main.mail.002@gmail.com>
2026-02-18 20:25:24 +0900

    feat: 基本的な算術ユーティリティ関数 (Add, PrintAdd, Sub, PrintSub) を追加

--- utils/utils.go
+++ utils/utils.go
@@ -9,3 +9,11 @@ func Add(a int, b int) int {
 func PrintAdd(a int, b int) {
 	fmt.Println(Add(a, b))
 }
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func PrintSub(a int, b int) {
+	fmt.Println(Sub(a, b))
+}
